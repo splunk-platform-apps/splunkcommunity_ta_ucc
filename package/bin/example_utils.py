27a5fd8cd91daecac3bba5882e2fc9296ef73a81
Erica Pescio <epescio@splunk.com>
2025-08-27 06:34:44 -0700

    chore:added src app files (#1)
    * chore:added src app files
    
    * ci:updated reusable workflows ref
    
    * chore(appinspect):updated manualcheck file
    
    ---------
    
    Co-authored-by: Erica Pescio <edro15@users.noreply.github.com>

---
+++ package/bin/example_utils.py
@@ -0,0 +1,6 @@
+ADDON_NAME = "SplunkCommunity_TA_UCCExample"
+
+
+def get_example_collection_key_name(input_name: str) -> str:
+    # `input_name` is a string like "example://<input_name>".
+    return input_name.split("/")[-1]
